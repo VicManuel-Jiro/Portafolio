bf5d4c62a3adf504c886b18dbd248167b2f2bc71
Victor Manuel Jimenez Rosas <vicmanuel2508@gmail.com>
2024-05-28 21:08:33 -0600

    actualizacion

--- assets/controllers/portafolioCards.js
+++ assets/controllers/portafolioCards.js
@@ -22,7 +22,7 @@ const portafolioCards = portafolioData.map(
           <a href=${data.repo}  target="_blank" rel="nofollow noreferrer noopener" ><h3>${data.title}</h3></a>
           <div class="btn__container">
             ${data.hasdemo ? `<a href=${data.demo} target="_blank" rel="nofollow noreferrer noopener" class="card__btn">Ver Demo</a>` : ''}
-            <a href=${data.repo}" target="_blank" rel="nofollow noreferrer noopener" class="card__btn">Ver repositorio</a>
+            <a href=${data.repo} target="_blank" rel="nofollow noreferrer noopener" class="card__btn">Ver repositorio</a>
           </div>
         </article>
  `
