96d3e24ecf8dd437d8d8ce65531b815e80e8da28
Victor Manuel Jimenez Rosas <vicmanuel2508@gmail.com>
2024-06-03 14:39:10 -0600

    actualizacion de estilos

--- modules.js
+++ modules.js
@@ -1,7 +1,7 @@
 import menuHamburger from "./assets/controllers/hamburger.js";
 
-import {portafolioContainer } from "./assets/controllers/portafolioCards.js?v=${new Date().getTime()}"
+import {portafolioContainer } from "./assets/controllers/portafolioCards.js"
 
 import menuActive from "./assets/controllers/header_activate.js";
 
-/*import { academicContainer } from "./assets/controllers/academicCards.js";*/
+/*import { academicContainer } from "./assets/controllers/academicCards.js?v=${new Date().getTime()}";*/
