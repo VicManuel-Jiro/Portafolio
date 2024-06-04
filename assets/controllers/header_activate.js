5bb26217db806ecd19dde594ac3c172735a6b851
Victor Manuel Jimenez Rosas <vicmanuel2508@gmail.com>
2024-06-03 18:04:21 -0600

    actualizacion de estilos

--- assets/controllers/header_activate.js
+++ assets/controllers/header_activate.js
@@ -1,14 +1,15 @@
-export default function menuActive(){
+export default function menuActive() {
     document.addEventListener('DOMContentLoaded', () => {
         const menuLinks = document.querySelectorAll('.menu__link');
     
         menuLinks.forEach(link => {
             link.addEventListener('click', (event) => {
+                event.preventDefault(); // Evitar el comportamiento por defecto
                 menuLinks.forEach(link => link.classList.remove('active'));
-                event.target.classList.add('active');
+                event.currentTarget.classList.add('active');
             });
         });
     });
-    
 }
+
 menuActive();
